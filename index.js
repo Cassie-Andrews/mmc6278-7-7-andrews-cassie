6056b459c0caf618ddb8b6aef1244b52619a6e5d
Cassie Andrews <candrews1@ufl.edu>
2025-03-14 17:08:49 -0400

    fix app.js

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const app = require("./app");
 const PORT = process.env.PORT || 3000;
+
 const session = require('express-session')
 const MySQLStore = require('express-mysql-session')(session);
 const db = require('./db')
+
+const sessionStore = new MySQLStore({}, db);
+app.use(session({
+	key: 'session_cookie',
+	secret: process.env.SESSION_SECRET,
+	store: sessionStore,
+	resave: false,
+	saveUninitialized: false,
+	proxy: true,
+	cookie: {
+		maxAge: 1000 * 60 * 60 * 24
+	}
+}));
 
 app.listen(PORT, function () {
   console.log("\n");
